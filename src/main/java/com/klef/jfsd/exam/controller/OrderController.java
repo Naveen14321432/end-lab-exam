a56cffc9a16fe591ae14269e3b23627afb0953eb
Naveen14321432 <naveenkammili04@gmail.com>
2024-12-07 12:24:00 +0530

    Initial commit

---
+++ src/main/java/com/klef/jfsd/exam/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Order;
+import com.klef.jfsd.exam.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        if (order.getOrderId() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(orderService.saveOrder(order), HttpStatus.CREATED);
+    }
+}
